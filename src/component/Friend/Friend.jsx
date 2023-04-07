eb3332afebc0bca3cafea1fe68fa3481ae2e38db
ruhulaminpollob <ruhulamin.pollob@gmail.com>
2023-04-08 02:29:33 +0600

    added useNavigate hook to use goBack methode

--- src/component/Friend/Friend.jsx
+++ src/component/Friend/Friend.jsx
@@ -1,14 +1,20 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import "./Friend.css"
 
 const Friend = ({friend}) => {
+    const navigate=useNavigate()
+    const showDetails=()=>{
+        navigate(`/friend/${friend.id}`)
+    }
     return (
         <div className='friend'>
             <h3>{friend.name}</h3>
             <p>email: {friend.email}</p>
             <p>Phone Number: {friend.phone}</p>
             <p> <Link to={`/friend/${friend.id}`}>See Details</Link> </p>
+
+            <button onClick={showDetails}>Details</button>
         </div>
     );
 };
--- src/component/FriendDetail/FriendDetail.jsx
+++ src/component/FriendDetail/FriendDetail.jsx
@@ -1,14 +1,19 @@
 import React from 'react';
-import { useLoaderData } from 'react-router-dom';
+import { useLoaderData, useNavigate } from 'react-router-dom';
 
 const FriendDetail = () => {
     const details=useLoaderData();
     console.log(details);
+    const navigate=useNavigate()
+    const navigateHandler=()=>{
+        navigate(-1)
+    }
     return (
         <div>
             <h1>Friend Detail is here </h1>
             <h2>{details.name}</h2>
             <p>{details.website}</p>
+            <button onClick={navigateHandler}>go back</button>
         </div>
     );
 };
